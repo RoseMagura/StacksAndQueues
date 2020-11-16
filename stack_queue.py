ad245854226780aa0117ad0f42dfe9818715bb55
RoseMagura <magurarm@guilford.edu>
2020-11-15 20:13:49 -0500

    Feat: Try to implement stack-based queue

---
+++ stack_queue.py
@@ -0,0 +1,54 @@
+# Here is our Stack Class
+
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def size(self):
+        return len(self.items)
+    
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if self.size()==0:
+            return None
+        else:
+            return self.items.pop()
+
+class Queue:
+    def __init__(self):
+        Stack.__init__(self)
+        
+    def size(self):
+        return Stack.size(self)
+        
+    def enqueue(self,item):
+        Stack.push(self, item)
+        
+    def dequeue(self):
+        value = self.items[0]
+        self.items = self.items[1:]
+        return value
+
+        
+
+# Setup
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+q.enqueue(3)
+
+# Test size
+print ("Pass" if (q.size() == 3) else "Fail")
+
+# Test dequeue
+print ("Pass" if (q.dequeue() == 1) else "Fail")
+
+# Test enqueue
+q.enqueue(4)
+print ("Pass" if (q.dequeue() == 2) else "Fail")
+print ("Pass" if (q.dequeue() == 3) else "Fail")
+print ("Pass" if (q.dequeue() == 4) else "Fail")
+q.enqueue(5)
+print ("Pass" if (q.size() == 1) else "Fail")
